e61e8837b36409bd79ddbda75ef11e11ca13893f
Ahmedullah Siddiqui <ahmedullah.siddiqui@tenpearls.com>
2020-06-15 09:43:23 +0500

    assignment-2b

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const http = require('http');
+const express = require("express")
+const bodyParser = require("body-parser")
+const userRoutes = require("./routes/user")
+const path = require("path")
 
-const routes = require('./routes');
+const app = express()
+const port = 3000
 
-const server = http.createServer(routes);
+app.use(bodyParser.urlencoded())
+app.use(express.static(path.join(__dirname, "css")))
 
-server.listen(3000);
+app.use('/user', userRoutes)
+
+app.get('/',(req, res, next) => {
+    res.sendFile(path.join(__dirname, "views", "main.html"))
+})
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "views", "404.html"))
+})
+app.listen(port, () => console.log(`Example app listening on port port!`))
